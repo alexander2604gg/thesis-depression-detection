7c1e5284bc3cc25d62d897eb665b5ab70c1f3b37
Alexander Saul <alexandersaul2604@gmail.com>
2025-11-12 00:30:09 -0500

    fix: cors

--- src/main/java/com/alexandersaul/Alexander/config/CorsConfig.java
+++ src/main/java/com/alexandersaul/Alexander/config/CorsConfig.java
@@ -13,11 +13,10 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")  // Aplica a todos los endpoints
-                        .allowedOrigins("http://18.117.10.88") // Tu frontend público
+                registry.addMapping("/**")               // Aplica a todos los endpoints
+                        .allowedOrigins("*")             // Permite todos los orígenes
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);  // Si Angular usa withCredentials
+                        .allowedHeaders("*");
             }
         };
     }
